3a78e36189068fa4ce560157746370c877531e56
codeass <admin@codeass.com>
2014-07-09 19:57:45 +0500

    Update assuploader.js

--- assuploader.js
+++ assuploader.js
@@ -16,12 +16,14 @@ $.fn.assuploader = function(options){
 	var ass = $.extend({
 			formats: ['jpg','bmp','gif','png'],
 			maxsize: 2,
+			debug: false,
 			border: '1px #07F solid',
 			radius: '2px',
 			padding: '10px', 
 			uploadurl: '',
 			uploadpath: './files/',
-			asstarget: ''
+			asstarget: '',
+			complete: function(){}
 	}, options);
 	
 	//Create the Default form
@@ -29,8 +31,8 @@ $.fn.assuploader = function(options){
 	+'text-align:center;font-family:sans-serif, arial, tahoma;font-size:14px;font-weight:bold;color:#999999;display:none;'
 	+'padding-top:13px;z-index:2;">Drop Files Here!</div>'
 	+'<div id="assuploaderfileinfo" '
-	+'style="position:absolute;top:1px;right:1px;bottom:1px;left:1px;background:rgba(255,255,255,1);text-align:left;font-family:sans-serif,' +'arial, tahoma;font-size:14px;font-weight:bold;color:#999999;display:none;z-index:1;padding:7px 10px;"><div id="asstitle"></div>'
-	+'<div id="assinfo" style="font-weight:normal;font-size:12px;"></div></div>'
+	+'style="position:absolute;top:1px;right:1px;bottom:1px;left:1px;background:rgba(255,255,255,1);text-align:left;font-family:sans-serif,' +'arial, tahoma;font-size:14px;font-weight:bold;color:#999999;display:none;z-index:1;padding:7px 10px;"><span id="asstitle"></span>'
+	+' <span id="assinfo" style="font-weight:normal;font-size:12px;"></span></div>'
 	+'<input type="file" id="assuploaderfile">'
 	+'<div id="asserror" style="font-weight:bold;color:#FD3535;font-family:sans-serif,arial,tahoma;font-size:14px;display:none;margin-top:5px;"></div>'
 	+ '<div id="assprogressbar" style="position:absolute;overflow:hidden;bottom:0px;left:0px;right:0px;height:4px;border-top: '+ass.border+';display:none;z-index:5;"><div id="assbar" style="background:#09F;height:5px;width:0%;"></div></div>';
@@ -42,7 +44,6 @@ $.fn.assuploader = function(options){
 	});
 	
 	//Handle the Drag and drop First
-	//var dropbox = this; 
 	this.on('dragenter', function(e){
     	e.stopPropagation();		e.preventDefault();
     	$("#assuploaderdropbox").fadeIn(200);
@@ -58,11 +59,6 @@ $.fn.assuploader = function(options){
  		$("#assuploaderdropbox").fadeOut(200);
 		e.preventDefault();
      	assInfo(e.originalEvent.dataTransfer.files[0]);
-     	//For now only single upload
-     	
-     	
-     	//We need to send dropped files to Server
-     	//handleFileUpload(files,this);
 	});
 	$(document).on('dragenter', function (e){
 		e.stopPropagation();     e.preventDefault();
@@ -98,14 +94,14 @@ $.fn.assuploader = function(options){
 		}else{
 			_input.attr("disabled", "true");
  			assify.css({ border: ass.border, background: "#FFFFFF"});
-			$("#assuploaderfileinfo div#asstitle").html(file.name);
+			$("#assuploaderfileinfo span#asstitle").html(file.name);
         	if(parseInt(sizeKB) > 1024){  var sizeMB = sizeKB/1024;  sizeStr = sizeMB.toFixed(2)+" MB";  }
         	else{  sizeStr = sizeKB.toFixed(2)+" KB"; }
- 			var filesinfo = 'Size: '+sizeStr;
- 			$("#assuploaderfileinfo div#assinfo").html(filesinfo);
+ 			var filesinfo = '('+sizeStr+')';
+ 			$("#assuploaderfileinfo span#assinfo").html(filesinfo);
  			$("#assuploaderfileinfo").fadeIn(200); 	
  			
- 			//Now Upload the file
+ 			//Upload the file
  			handleFileUpload(file, assify);
  		}
 	}
@@ -119,7 +115,7 @@ $.fn.assuploader = function(options){
 	}
 
 	function sendFileToServer(formData,status){
-    var extraData ={}; //Extra Data.
+    var extraData ={};
     var jqXHR=$.ajax({
             xhr: function() {
             var xhrobj = $.ajaxSettings.xhr();
@@ -131,10 +127,8 @@ $.fn.assuploader = function(options){
                         if (event.lengthComputable) {
                             percent = Math.ceil(position / total * 100);
                         }
-                        //Set progress
                         var progressBarWidth = percent * _assprogress.width() / 100;  
-                        _assprogress.find('div').animate({ width: progressBarWidth }, 200); //.html(progress + "% ");
-                        //status.setProgress(percent);
+                        _assprogress.find('div').animate({ width: progressBarWidth }, 500);
                     }, false);
                 }
             return xhrobj;
@@ -146,7 +140,7 @@ $.fn.assuploader = function(options){
         cache: false,
         data: formData,
         success: function(data){
-        	//console.log(data);
+        	if(ass.debug){ console.log(data); }
         	var json = $.parseJSON(data);
         	if(data.result=="failed"){
         		_input.removeAttr("disabled");
@@ -155,14 +149,15 @@ $.fn.assuploader = function(options){
 				assify.css({ border: "1px solid #FFC8C8", background: "#FFE9E9"});
         		_assprogress.hide();
         	}else{
-        		$("#"+ass.asstarget).attr("value", json.filename);
+        		$("#"+ass.asstarget).val(json.filename);
         		_assprogress.find('div').css("background", "#73EB78");
-        		_assprogress.find('div').animate({ width: '100%'}, 200); 
+        		_assprogress.find('div').animate({ width: '100%'}, 500); 
+        		ass.complete();
         	}
         },
         error: function(){
         	_assprogress.find('div').css("background", "#EB7373");
-        	_assprogress.find('div').animate({ width: '100%'}, 200); //.html(progress + "% "); 
+        	_assprogress.find('div').animate({ width: '100%'}, 500);
         }
     }); 
 
