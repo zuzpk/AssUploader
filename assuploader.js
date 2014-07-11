304aba8c498c00cb9f9333e1504dbfa373587255
codeass <admin@codeass.com>
2014-07-11 05:00:37 +0500

    Update assuploader.js

--- assuploader.js
+++ assuploader.js
@@ -12,7 +12,7 @@
 	
 $.fn.assuploader = function(options){
 	//Default Settings
-	var assify = this;
+	var assify = this, assified = 0, prevass = 0, speedass = false, speed = 0, total = 0, remainingBytes = 0, timeRemaining = 0;
 	var ass = $.extend({
 			formats: ['jpg','bmp','gif','png'],
 			maxsize: 2,
@@ -23,6 +23,7 @@ $.fn.assuploader = function(options){
 			uploadurl: '',
 			uploadpath: './files/',
 			asstarget: '',
+			showspeed: false,
 			complete: function(){}
 	}, options);
 	
@@ -34,7 +35,7 @@ $.fn.assuploader = function(options){
 	+'style="position:absolute;top:1px;right:1px;bottom:1px;left:1px;background:rgba(255,255,255,1);text-align:left;font-family:sans-serif,' +'arial, tahoma;font-size:14px;font-weight:bold;color:#999999;display:none;z-index:1;padding:7px 10px;"><span id="asstitle"></span>'
 	+' <span id="assinfo" style="font-weight:normal;font-size:12px;"></span></div>'
 	+'<input type="file" id="assuploaderfile">'
-	+'<div id="asserror" style="font-weight:bold;color:#FD3535;font-family:sans-serif,arial,tahoma;font-size:14px;display:none;margin-top:5px;"></div>'
+	+'<div id="asserror" style="font-weight:bold;color:#FD3535;font-family:sans-serif,arial,tahoma;font-size:14px;display:none;margin-top:5px;"></div><div id="assuploadspeed" style="display:none;color:#777;z-index:2;position:absolute;right:10px;bottom:10px;"></div>'
 	+ '<div id="assprogressbar" style="position:absolute;overflow:hidden;bottom:0px;left:0px;right:0px;height:4px;border-top: '+ass.border+';display:none;z-index:5;"><div id="assbar" style="background:#09F;height:5px;width:0%;"></div></div>';
 	this.html(_form);	
 	this.css({
@@ -106,11 +107,29 @@ $.fn.assuploader = function(options){
  		}
 	}
 	
+	function UploadSpeed(){
+		//speed
+		speed = assified - prevass;
+		prevass = assified;
+		var speedStr;
+		if(parseInt(speed) > 1024){  
+			speed = speed / 1024;
+			if(speed>=1024){
+				speedStr = speed+' mb / sec';
+			}else{
+				speedStr = speed + ' kb / sec';
+			}
+		} 
+		$("#assuploadspeed").html(speedStr);
+	}
+	
 	function handleFileUpload(files,obj){
 		var fd = new FormData();
         fd.append('file', files);
         fd.append('uploadpath', ass.uploadpath);
-        _assprogress.show();   
+        _assprogress.show();
+        $("#assuploadspeed").show();   
+        if(ass.showspeed){ speedass = setInterval(UploadSpeed, 1000); }
         sendFileToServer(fd, status);
 	}
 
@@ -126,6 +145,7 @@ $.fn.assuploader = function(options){
                         var total = event.total;
                         if (event.lengthComputable) {
                             percent = Math.ceil(position / total * 100);
+                            assified = event.loaded;
                         }
                         var progressBarWidth = percent * _assprogress.width() / 100;  
                         _assprogress.find('div').animate({ width: progressBarWidth }, 500);
@@ -140,6 +160,7 @@ $.fn.assuploader = function(options){
         cache: false,
         data: formData,
         success: function(data){
+        	$("#assuploadspeed").hide();
         	if(ass.debug){ console.log(data); }
         	var json = $.parseJSON(data);
         	if(data.result=="failed"){
@@ -151,7 +172,8 @@ $.fn.assuploader = function(options){
         	}else{
         		$("#"+ass.asstarget).val(json.filename);
         		_assprogress.find('div').css("background", "#73EB78");
-        		_assprogress.find('div').animate({ width: '100%'}, 500); 
+        		_assprogress.find('div').animate({ width: '100%'}, 500);
+        		clearInterval(speedass); 
         		ass.complete();
         	}
         },
